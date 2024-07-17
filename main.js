623439c745c93a5f073a6aaca01b8fa7cd041c4b
Brandon Gowera <gowerabrandon@gmail.com>
2024-07-17 09:15:18 +0200

    added userInput variable and getComputerChoice function

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Let's play rock, paper, scissors!")
+
+//Get user input
+//let playerChoice = prompt("Rock,paper or scissors?").toLowerCase();
+
+//Create randomized computer choices
+
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let randomNumber = parseInt(Math.floor(Math.random() * 3))
+    let computerChoice;
+
+    if (randomNumber == 0) {
+        computerChoice = choices[0];
+    } else if (randomNumber == 1) {
+        computerChoice = choices[1];
+    } else {
+        computerChoice = choices[2];
+    }
+
+    return computerChoice;
+}
+
+
+
