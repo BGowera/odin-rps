62b0e93a3c1703cc525c3f0b1d27fb659a5cfd10
Brandon Gowera <gowerabrandon@gmail.com>
2024-07-21 12:43:15 +0200

    Added if/else statement to compare player and computer choices

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
-console.log("Let's play rock, paper, scissors!")
+console.log("Let's play rock, paper, scissors!");
 
 //Get user input
-//let playerChoice = prompt("Rock,paper or scissors?").toLowerCase();
+let playerChoice = prompt("Rock,paper or scissors?").toLowerCase();
 
 //Create randomized computer choices
 
 function getComputerChoice() {
-    let choices = ['rock', 'paper', 'scissors'];
-    let randomNumber = parseInt(Math.floor(Math.random() * 3))
-    let computerChoice;
+	let choices = ["rock", "paper", "scissors"];
+	let randomNumber = parseInt(Math.floor(Math.random() * 3));
+	let computerChoice;
 
-    if (randomNumber == 0) {
-        computerChoice = choices[0];
-    } else if (randomNumber == 1) {
-        computerChoice = choices[1];
-    } else {
-        computerChoice = choices[2];
-    }
+	if (randomNumber == 0) {
+		computerChoice = choices[0];
+	} else if (randomNumber == 1) {
+		computerChoice = choices[1];
+	} else {
+		computerChoice = choices[2];
+	}
 
-    return computerChoice;
+	return computerChoice;
 }
 
 //compare user choice and computer choice
 
-function getRoundWinner() {
-    let winner;
-    let computerChoice = getComputerChoice();
-    if (playerChoice == "rock" && computerChoice == "paper") {
-        winner=`Oh no, looks like the computer won this round!`;
-}else if()
-
-    return winner;
+let roundWinner;
+let computerChoice = getComputerChoice();
+if (
+	(playerChoice == "rock" && computerChoice == "scissors") ||
+	(playerChoice == "paper" && computerChoice == "rock") ||
+	(playerChoice == "scissors" && computerChoice == "paper")
+) {
+	roundWinner = "Congratulations, you won this round!";
+} else if (
+	(playerChoice == "rock" && computerChoice == "paper") ||
+	(playerChoice == "paper" && computerChoice == "scissors") ||
+	(playerChoice == "scissors" && computerChoice == "rock")
+) {
+	roundWinner = "Too bad, the computer wins this time!";
+} else {
+    roundWinner='Looks like a draw!'
 }
 
+//
 
+console.log(playerChoice);
+console.log(computerChoice);
+console.log(roundWinner);
