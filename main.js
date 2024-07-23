e36f4e5ac6b4c7b8d70720ad9f66670e88006245
Brandon Gowera <gowerabrandon@gmail.com>
2024-07-23 04:52:47 +0200

    little variable to confirm if player wants to play

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 //****************TO DO */
-// Fix playGame() function to play more rounds
-// Add a way to check the overall winner to the game
-
-
+// Figure out user input validation or just fix it in the GUI
+// Maybe shorten some of the spaghetti code
 
 //Get user input
 function getPlayerChoice() {
-    let choice = prompt('Rock, paper,or scissors?').toLowerCase();
+	let choice = prompt("Rock, paper,or scissors?").toLowerCase();
 
-    return choice;
+	return choice;
 }
 
 //Create randomized computer choices
@@ -34,59 +32,63 @@ function getComputerChoice() {
 let computerScore = 0;
 let playerScore = 0;
 
-
 function playRound() {
+	let computerChoice = getComputerChoice();
+	let playerChoice = getPlayerChoice();
 
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
+	let roundWinner;
 
-    let roundWinner;
-
-    if (
-        (playerChoice == "rock" && computerChoice == "scissors") ||
-        (playerChoice == "paper" && computerChoice == "rock") ||
-        (playerChoice == "scissors" && computerChoice == "paper")
-    ) {
-        roundWinner = `You played ${playerChoice} and the computer played ${computerChoice} so you win!`;
-        playerScore++
-    } else if (
-        (playerChoice == "rock" && computerChoice == "paper") ||
-        (playerChoice == "paper" && computerChoice == "scissors") ||
-        (playerChoice == "scissors" && computerChoice == "rock")
-    ) {
-        roundWinner = `You played ${playerChoice} and the computer played ${computerChoice} so the computer wins!`;
-        computerScore++;
-    } else {
-        roundWinner = `You played ${playerChoice} and the computer played ${computerChoice} so this round was a draw!`;
-    }
-    return roundWinner;
+	if (
+		(playerChoice == "rock" && computerChoice == "scissors") ||
+		(playerChoice == "paper" && computerChoice == "rock") ||
+		(playerChoice == "scissors" && computerChoice == "paper")
+	) {
+		roundWinner = `You played ${playerChoice} and the computer played ${computerChoice} so you win!`;
+		playerScore++;
+	} else if (
+		(playerChoice == "rock" && computerChoice == "paper") ||
+		(playerChoice == "paper" && computerChoice == "scissors") ||
+		(playerChoice == "scissors" && computerChoice == "rock")
+	) {
+		roundWinner = `You played ${playerChoice} and the computer played ${computerChoice} so the computer wins!`;
+		computerScore++;
+	} else {
+		roundWinner = `You played ${playerChoice} and the computer played ${computerChoice} so this round was a draw!`;
+	}
+	return roundWinner;
 }
+
+// check who won the game
 
 function checkWinner() {
-    let winner;
-    if (playerScore > computerScore) {
-        winner = "Player";
-    } else if (computerScore > playerScore) {
-        winner = "Computer";
-    } else {
-        winner="Nobody - it was a draw"
-    }
+	let winner;
+	if (playerScore > computerScore) {
+		winner = "Player";
+	} else if (computerScore > playerScore) {
+		winner = "Computer";
+	} else {
+		winner = "Nobody - it was a draw";
+	}
 
-    return `Winner:${winner}`;
+	return `Winner:${winner}`;
 }
+
+// function to run a five round game
 
 function playGame() {
-
-    for (let i = 1; i < 6; i++){
-        console.log(`Round ${i}`)
-        console.log(`Your score:${playerScore}`);
-        console.log(`Computer score:${computerScore}`)
-        console.log(playRound());
-    }
-       console.log(`Final score`);
+	for (let i = 1; i < 6; i++) {
+		console.log(`Round ${i}`);
 		console.log(`Your score:${playerScore}`);
-    console.log(`Computer score:${computerScore}`);
-    console.log(checkWinner());
+		console.log(`Computer score:${computerScore}`);
+		console.log(playRound());
+	}
+	console.log(`Final score`);
+	console.log(`Your score:${playerScore}`);
+	console.log(`Computer score:${computerScore}`);
+	console.log(checkWinner());
 }
 
-playGame();
+let confirmation = prompt("Do you want to play rock, paper, scissors? Yes or No?").toLowerCase();
+
+confirmation == "yes" ? playGame() : alert("See ya!");
+
