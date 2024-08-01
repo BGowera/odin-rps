241af409217062ff23a8018ec0053e9d8cad3d95
Brandon Gowera <gowerabrandon@gmail.com>
2024-08-01 17:49:09 +0200

    made checkwinner function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//DOM Elements
+// DOM Elements
 const playerScorePara = document.querySelector(".player-score-para");
 const computerScorePara = document.querySelector(".computer-score-para");
 const roundNumberHeading = document.querySelector(".round-number-heading");
@@ -8,59 +8,75 @@ const scissorsBtn = document.querySelector(".scissors-btn");
 const computerChoicePara = document.querySelector(".computer-choice-para");
 const roundWinnerPara = document.querySelector(".round-winner-para");
 
-computerChoicePara.textContent = "";
-roundWinnerPara.textContent = "";
-roundNumberHeading.textContent = "";
+computerChoicePara.innerText = "";
+roundWinnerPara.innerText = "";
+roundNumberHeading.innerText = "";
 
 let computerScore = 0;
 let playerScore = 0;
 let playerChoice;
 let computerChoice;
 let roundNumber = 1;
+let turnsLeft = 5;
 
-
-
-
-
-//functions
+// Functions
 
 function getComputerChoice() {
 	let choices = ["rock", "paper", "scissors"];
-	let randomNumber = parseInt(Math.floor(Math.random() * 3));
-	let computerChoice;
-
-	if (randomNumber == 0) {
-		computerChoice = choices[0];
-	} else if (randomNumber == 1) {
-		computerChoice = choices[1];
-	} else {
-		computerChoice = choices[2];
-	}
-
-	return computerChoice;
+	let randomNumber = Math.floor(Math.random() * 3);
+	return choices[randomNumber];
 }
-
 
 function updateScore() {
-	roundNumberHeading.textContent = `Round ${roundNumber}! Rock, paper or scissors?`;
-	playerScorePara.textContent = `Player Score:${playerScore}`;
-	computerScorePara.textContent = `Computer Score:${computerScore}`;
+	roundNumberHeading.innerText = `Round ${roundNumber}! Rock, paper or scissors?`;
+	playerScorePara.innerText = `Player Score: ${playerScore}`;
+	computerScorePara.innerText = `Computer Score: ${computerScore}`;
 }
-
-
 
 function capitalizeFirstLetter(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+
+
+function checkWinner() {
+	if (playerScore > computerScore) {
+		console.log("Player wins");
+		roundNumberHeading.innerText = `Congratulations! You win!`;
+		document.body.style.backgroundColor = "green";
+
+	} else if (computerScore > playerScore) {
+		console.log("Computer wins");
+		roundNumberHeading.innerText = `Sorry! The computer won this one!`;
+		document.body.style.backgroundColor = "red";
+	
+	} else {
+		console.log("It's a tie");
+		roundNumberHeading.innerText = `This game ended on a tie`;
+		document.body.style.backgroundColor = "#e2e2e2";
+	}
+	rockBtn.disabled = true;
+	paperBtn.disabled = true;
+	scissorsBtn.disabled = true;
+}
+
+
 function playRound() {
-	roundNumber++;
-		if (
+		turnsLeft--;
+roundNumber++;
+console.log(turnsLeft, roundNumber)
+	if (turnsLeft == 0) {
+		checkWinner();
+		return
+	}
+	if (
 		(playerChoice == "rock" && computerChoice == "scissors") ||
 		(playerChoice == "paper" && computerChoice == "rock") ||
 		(playerChoice == "scissors" && computerChoice == "paper")
 	) {
-		computerChoicePara.textContent = `Computer played ${computerChoice}. ${capitalizeFirstLetter(playerChoice)} beats ${computerChoice}.`;
+		computerChoicePara.innerText = `Computer played ${computerChoice}. ${capitalizeFirstLetter(
+			playerChoice
+		)} beats ${computerChoice}.`;
 		playerScore++;
 		document.body.style.backgroundColor = "green";
 	} else if (
@@ -69,44 +85,42 @@ function playRound() {
 		(playerChoice == "scissors" && computerChoice == "rock")
 	) {
 		computerScore++;
-		computerChoicePara.textContent = `Computer played ${computerChoice}. ${capitalizeFirstLetter(computerChoice)} beats ${playerChoice}.`;
+		computerChoicePara.innerText = `Computer played ${computerChoice}. ${capitalizeFirstLetter(
+			computerChoice
+		)} beats ${playerChoice}.`;
 		document.body.style.backgroundColor = "red";
 	} else {
-		computerChoicePara.textContent = `Computer played ${computerChoice}. This round was a draw.`;
+		computerChoicePara.innerText = `Computer played ${computerChoice}. This round was a draw.`;
 		document.body.style.backgroundColor = "#e2e2e2";
+		
 	}
+	
+	updateScore()
 }
-console.log(roundNumber)
-function playRock() {
 
+
+function playRock() {
 	playerChoice = "rock";
 	computerChoice = getComputerChoice();
 	playRound();
-	updateScore();
-
 }
 
 function playPaper() {
 	playerChoice = "paper";
 	computerChoice = getComputerChoice();
 	playRound();
-	updateScore();
 }
 
 function playScissors() {
 	playerChoice = "scissors";
 	computerChoice = getComputerChoice();
 	playRound();
-	updateScore();
 }
 
-if (roundNumber > 5) {
-	alert("hello");
-}
 
 
 updateScore();
-//event listeners
+// Event listeners
 
 rockBtn.addEventListener("click", playRock);
 paperBtn.addEventListener("click", playPaper);
